3c293e6cbec59fddfc61b3a356eb12006003449d
ArtemParhom <112825602+ArtemParhom@users.noreply.github.com>
2023-02-21 15:37:02 +0000

    Deploying to gh-pages from @ ArtemParhom/goit-js-hw-08@23ce8eb3fbe9d9be43e0ff3ada50dfb208f24ad7 🚀

--- 02-video.168acc53.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("iframe"),t=new Vimeo.Player(e);t.on("play",(function(){console.log("played the video!")})),t.getVideoTitle().then((function(e){console.log("title:",e)}));t.on("timeupdate",(function(e){localStorage.setItem("videoplayer-current-time",e.seconds),console.log(localStorage.getItem("videoplayer-current-time"))})),t.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then((function(e){})).catch((function(e){e.name}))}();
-//# sourceMappingURL=02-video.168acc53.js.map
--- 02-video.77cf54b1.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("iframe"),t=new Vimeo.Player(e);t.on("play",(function(){console.log("played the video!")})),t.getVideoTitle().then((function(e){console.log("title:",e)}));t.on("timeupdate",(function(e){localStorage.setItem("videoplayer-current-time",e.seconds),console.log(localStorage.getItem("videoplayer-current-time"))})),t.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then((function(e){})).catch((function(e){e.name}));
-//# sourceMappingURL=02-video.77cf54b1.js.map
